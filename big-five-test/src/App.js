bf4123971bbbc8cc1c10a1d9063055b432d8423b
Deharath <gracjando@gmail.com>
2023-04-18 19:37:58 +0200

    Project has been set up.

---
+++ big-five-test/src/App.js
@@ -0,0 +1,13 @@
+import React, { useState } from 'react';
+import Welcome from './components/Welcome/welcome.jsx';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <Welcome/>
+    </div>
+  );
+}
+
+export default App;
---
+++ big-five-test/src/components/Welcome/welcome.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import styles from './welcome.css';
+
+export default function Welcome(){
+  return (
+    <div id="main">
+      <h1>Welcome to the Big Five Personality Test</h1>
+      <p>Click the button below to start the questionnaire.</p>
+      <button>Start</button>
+    </div>
+  );
+};
+
